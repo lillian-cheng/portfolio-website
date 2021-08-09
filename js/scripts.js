5678407c0417bb4b98a3027e6fb7b8aa47fc1553
lillianwcheng <87872159+lillianwcheng@users.noreply.github.com>
2021-08-09 15:29:45 -0400

    applied hamburger menu

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+console.log("Hi! Welcome to my Portfolio Site :)")
+function menuToggle(){
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+} else {
+  x.className = 'navtoggle';
+}
