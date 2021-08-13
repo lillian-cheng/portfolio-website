702790dd81b37250589f3b5e97b9a5027509b776
lillianwcheng <87872159+lillianwcheng@users.noreply.github.com>
2021-08-13 13:24:55 -0400

    updated pages

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,19 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 }
+
+$('.slider-for').slick({
+ slidesToShow: 1,
+ slidesToScroll: 1,
+ arrows: false,
+ fade: true,
+ asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+ slidesToShow: 3,
+ slidesToScroll: 1,
+ asNavFor: '.slider-for',
+ dots: true,
+ centerMode: true,
+ focusOnSelect: true
+});
