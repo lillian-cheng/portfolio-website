133d689c905b55857520700f51832449c5a1e38b
lillianwcheng <87872159+lillianwcheng@users.noreply.github.com>
2021-08-23 20:38:19 -0400

    adjustments made towards the whole site

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,9 @@
-console.log("Hi! Welcome to my Portfolio Site :)")
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
-
-$('.slider-for').slick({
- slidesToShow: 1,
- slidesToScroll: 1,
- arrows: false,
- fade: true,
- asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
- slidesToShow: 3,
- slidesToScroll: 1,
- asNavFor: '.slider-for',
- dots: true,
- centerMode: true,
- focusOnSelect: true
-});
